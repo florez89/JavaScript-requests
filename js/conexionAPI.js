024b9faa073388a848a458f092a83f6055fb965c
Gustavo Florez <florezg217@gmail.com>
2024-05-15 16:14:18 -0300

    pasos hasta modulo 2, clase 5

---
+++ js/conexionAPI.js
@@ -0,0 +1,17 @@
+async function listarVideos(){
+    const conexion = await fetch("http://localhost:3001/videos");
+    
+    const conexionConvertida = conexion.json();
+
+    //console.log(conexionConvertida);
+
+    return conexionConvertida 
+}
+
+export const conectaAPI={
+    listarVideos
+}
+
+//listarVideos();
+
+
---
+++ js/mostrarVideos.js
@@ -0,0 +1,25 @@
+import { conexionAPI } from "./conexionAPI";
+
+const lista = document.querySelector("[data-lista]");
+
+function crearCard(titulo,descripcion,url,imagen){
+    const video = document.createElement("li");
+    video.className="videos__item";
+    video.innerHTML=`<iframe width="100%" height="72%" src="${url}"
+    title="${titulo}"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
+<div class="descripcion-video">
+    <img src="${imagen}">
+    <h3>${titulo}</h3>
+    <p>${descripcion}</p>
+</div>`;
+
+return video;
+}
+
+async function listarVideos(){
+    const listaAPI = await conectaAPI.listarVideos()
+
+    listaAPI.foreach(video=>lista.appendChild(crearCard(video.titulo,video.descripcion,video.url,video.imagen)))
+}
