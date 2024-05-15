d20c1cf370a32c32b94fc972adfd821a9a353c44
Gustavo Florez <florezg217@gmail.com>
2024-05-15 17:16:17 -0300

    Termina modulo 2

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -1,25 +1,28 @@
-import { conexionAPI } from "./conexionAPI";
+import { conectaAPI } from "./conectaAPI.js";
 
 const lista = document.querySelector("[data-lista]");
 
-function crearCard(titulo,descripcion,url,imagen){
+function construyeCard(titulo,descripcion,url,imagen){
     const video = document.createElement("li");
     video.className="videos__item";
+    
     video.innerHTML=`<iframe width="100%" height="72%" src="${url}"
-    title="${titulo}"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    title="${titulo}" frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; geolocation"
     allowfullscreen></iframe>
 <div class="descripcion-video">
-    <img src="${imagen}">
+    <img src="${imagen}" alt="logo canal alura">
     <h3>${titulo}</h3>
     <p>${descripcion}</p>
-</div>`;
+</div>`
 
-return video;
+    return video;
 }
 
-async function listarVideos(){
-    const listaAPI = await conectaAPI.listarVideos()
 
-    listaAPI.foreach(video=>lista.appendChild(crearCard(video.titulo,video.descripcion,video.url,video.imagen)))
+async function listaVideos(){
+    const listaAPI = await conectaAPI.listaVideos();
+    listaAPI.forEach(element => lista.appendChild(construyeCard(element.titulo,element.descripcion,element.url,element.imagen)));
 }
+
+listaVideos();
